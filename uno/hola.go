35563c1c408f0c91d0ac8d784d9faecde5e5a2e4
ck <thecurrencykid@gmail.com>
2016-11-10 20:28:36 -0500

    scope

--- uno/hola.go
+++ uno/hola.go
@@ -2,16 +2,30 @@ package main
 
 import (
   "fmt"
-  "holaworld/stringutil" 
-  "holaworld/conversion"
+//  "holaworld/stringutil" 
+ // "holaworld/conversion"
 )
 
+var x = 0
 
 func main() {
-  p:= "this is the letter p" 
-	fmt.Println("Hello world!" + p)
-	fmt.Println(&p)
-	fmt.Println(stringutil.Name, stringutil.LastName)
-	fmt.Println(stringutil.Testy())
-  conversion.Convert()
+//  p:= "this is the letter p" 
+//	fmt.Println("Hello world!" + p)
+//	fmt.Println(&p)
+//	fmt.Println(stringutil.Name, stringutil.LastName)
+//	fmt.Println(stringutil.Testy())
+//  conversion.Convert()
+fmt.Println(increment())
+fmt.Println(x)
+
+fmt.Println(printXplusOne())
+}
+
+func increment() int {
+  x++
+  return x
+}
+
+func printXplusOne() int {
+  return x + 1
 }
